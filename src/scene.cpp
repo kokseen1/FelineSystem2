bdae7268eb13dd1466da45c5a48968458916ce4d
kokseen1 <53458630+kokseen1@users.noreply.github.com>
2022-11-18 23:15:42 +0800

    update: improve text cleaning

--- src/scene.cpp
+++ src/scene.cpp
@@ -144,6 +144,7 @@ std::string SceneManager::cleanText(const std::string &rawText)
     std::string text = std::regex_replace(sj2utf8(rawText), std::regex("\\[(.*?)\\]"), "$1");
     text = std::regex_replace(text, std::regex("¥fn"), "");
     text = std::regex_replace(text, std::regex("¥fs"), " ");
+    text = std::regex_replace(text, std::regex("¥n"), " ");
     text = std::regex_replace(text, std::regex("¥@"), "");
     // text = std::regex_replace(text, std::regex("\\\\fn"), "");
     // text = std::regex_replace(text, std::regex("\\\\fs"), "");
