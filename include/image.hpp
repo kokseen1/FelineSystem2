704598a4f941ee0584ea848a608b22f1a406289b
kose <53458630+kokseen1@users.noreply.github.com>
2022-12-12 21:23:42 +0800

    refactor: move classes to new file

--- include/image.hpp
+++ include/image.hpp
@@ -3,6 +3,7 @@
 #include <hgdecoder.hpp>
 #include <file.hpp>
 #include <scene.hpp>
+#include <imgtypes.hpp>
 
 #include <asmodean.h>
 #include <SDL2/SDL.h>
@@ -18,26 +19,6 @@
 
 #define IMAGE_EXT ".hg3"
 #define IMAGE_SIGNATURE "HG-3"
-
-// Flip mode when rendering image assets
-#define RENDERER_FLIP_MODE SDL_FLIP_VERTICAL
-
-#define WINDOW_WIDTH 1024
-#define WINDOW_HEIGHT 576
-
-// #define WINDOW_WIDTH 1280
-// #define WINDOW_HEIGHT 720
-
-#define FONT_PATH ASSETS "font.ttf"
-#define FONT_SIZE 20
-#define SELECT_FONT_PATH FONT_PATH
-#define SELECT_FONT_SIZE 30
-
-#define SEL "sys_sel_02"
-#define SEL_WIDTH 782
-#define SEL_HEIGHT 63
-#define SEL_SPACING 30
-#define SEL_XSHIFT WINDOW_WIDTH / 2 - SEL_WIDTH / 2
 
 #define MWND "sys_mwnd_43"
 #define MWND_DECO "sys_mwnd_42"
@@ -58,169 +39,12 @@
 #define SPEAKER_XPOS TEXT_XPOS
 #define SPEAKER_YPOS TEXT_YPOS - 25
 
-#define MAX_BG 10
-#define MAX_EG 10
-#define MAX_CG 10
-#define MAX_FW 10
-
-#define FW_XSHIFT 90
-#define FW_YSHIFT 160
-
-#define KEY_BG "bg"
-#define KEY_EG "eg"
-#define KEY_CG "cg"
-#define KEY_FW "fw"
-#define KEY_NAME "name"
-#define KEY_XSHIFT "x"
-#define KEY_YSHIFT "y"
-
-typedef std::pair<SDL_Texture *, Stdinfo> TextureData;
-
-extern std::unordered_map<std::string, TextureData> textureCache;
-
 enum class IMAGE_TYPE
 {
     BG,
     CG,
     EG,
     FW,
-};
-
-class Image
-{
-public:
-    std::string textureName;
-
-    // Additional offset applied on top of base position
-    int xShift = 0;
-    int yShift = 0;
-
-    Image(){};
-
-    Image(SDL_Renderer *, std::string, int, int);
-
-    bool isActive() { return !textureName.empty(); }
-
-    void render();
-
-    void clear();
-
-    const json dump();
-
-protected:
-    SDL_Renderer *renderer = NULL;
-
-    void render(const int, const int);
-
-private:
-    TextureData *textureData = NULL;
-};
-
-class Choice : public Image
-{
-private:
-    void renderText(const int, const int);
-
-public:
-    const std::string target;
-    const std::string prompt;
-
-    Choice(SDL_Renderer *, const std::string &, const std::string &);
-
-    void render(const int);
-};
-
-class Bg : public Image
-{
-    // Inherit ctors
-    using Image::Image;
-};
-
-class Eg : public Image
-{
-    // Inherit ctors
-    using Image::Image;
-};
-
-class Cg
-{
-public:
-    void render();
-
-    void clear();
-
-    bool isActive() { return !assetRaw.empty(); }
-
-    const json dump();
-
-    Image base;
-    Image part1;
-    Image part2;
-
-    std::string assetRaw;
-};
-
-class Fw : public Cg
-{
-public:
-    const json dump();
-};
-
-typedef struct
-{
-    const Image &image;
-    const std::string name; // Cannot be a reference in async
-    const int index;
-} ImageData;
-
-template <typename _Tp, std::size_t _Nm>
-class ImageLayer
-{
-private:
-    std::array<_Tp, _Nm> objects;
-
-public:
-    _Tp &operator[](size_t i) { return objects[i]; }
-
-    constexpr size_t size() { return objects.size(); }
-
-    const _Tp &get(size_t i)
-    {
-        if (i >= size())
-            return {};
-        return objects[i];
-    }
-
-    const json dump()
-    {
-        json j;
-        for (int i = 0; i < size(); i++)
-        {
-            if (!objects[i].isActive())
-                continue;
-            j[std::to_string(i)] = objects[i].dump();
-        }
-        return j;
-    }
-
-    void clear(size_t i)
-    {
-        if (i >= size())
-            return;
-        objects[i].clear();
-    }
-
-    void clear()
-    {
-        for (_Tp &image : objects)
-            image.clear();
-    }
-
-    void render()
-    {
-        for (_Tp &image : objects)
-            image.render();
-    }
 };
 
 class ImageManager
--- src/image.cpp
+++ src/image.cpp
@@ -8,6 +8,15 @@
 #include <sstream>
 #include <vector>
 
+// Clear the entire canvas
+void ImageManager::clearCanvas()
+{
+    clearImageType(IMAGE_TYPE::BG);
+    clearImageType(IMAGE_TYPE::EG);
+    clearImageType(IMAGE_TYPE::CG);
+    clearImageType(IMAGE_TYPE::FW);
+}
+
 void to_json(json &j, const Image &i)
 {
     j = json{
@@ -50,117 +59,10 @@ void from_json(const json &j, Cg &i)
     j.at(KEY_YSHIFT).get_to(i.base.yShift);
 }
 
-Image::Image(SDL_Renderer *r, std::string n, int x, int y) : renderer{r}, textureName{n}, xShift{x}, yShift{y}
-{
-}
-
-void Image::clear()
-{
-    textureName.clear();
-}
-
-Choice::Choice(SDL_Renderer *r, const std::string &t, const std::string &p) : Image(r, SEL, 0, 0), target(t), prompt(p)
-{
-}
-
-void Choice::render(const int yShift)
-{
-    // Render base box image
-    Image::render(SEL_XSHIFT, yShift);
-    renderText(SEL_XSHIFT, yShift);
-}
-
-// Render image with given offsets
-void Image::render(const int xShift, const int yShift)
-{
-    if (!isActive())
-        return;
-
-    if (textureData == NULL)
-    {
-        // Look for texture in cache
-        auto got = textureCache.find(textureName);
-        if (got == textureCache.end())
-            return;
-
-        textureData = &got->second;
-    }
-
-    auto texture = textureData->first;
-    if (texture == NULL)
-    {
-        LOG << "NULL texture in cache";
-        return;
-    }
-
-    // Calculate shift
-    auto &stdinfo = textureData->second;
-    auto xPos = stdinfo.OffsetX - stdinfo.BaseX + xShift;
-    auto yPos = stdinfo.OffsetY - stdinfo.BaseY + yShift;
-
-    // Render onto canvas
-    SDL_Rect DestR{xPos, yPos, static_cast<int>(stdinfo.Width), static_cast<int>(stdinfo.Height)};
-    SDL_RenderCopyEx(renderer, texture, NULL, &DestR, 0, 0, RENDERER_FLIP_MODE);
-}
-
-// Render image with the offsets set in ctor
-void Image::render()
-{
-    render(xShift, yShift);
-}
-
 const json Image::dump()
 {
     auto &j = *this;
     return j;
-}
-
-// Render text for a selection box
-void Choice::renderText(const int xShift, const int yShift)
-{
-    static TTF_Font *selectFont = TTF_OpenFont(SELECT_FONT_PATH, SELECT_FONT_SIZE);
-
-    if (prompt.empty())
-        return;
-
-    // Render text
-    SDL_Surface *surface = TTF_RenderUTF8_Blended_Wrapped(selectFont, prompt.c_str(), {0, 0, 0, 255}, 0);
-    if (surface == NULL)
-    {
-        LOG << "TTF Surface failed!";
-        return;
-    }
-
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-    if (texture == NULL)
-    {
-        LOG << "TTF texture failed!";
-        SDL_FreeSurface(surface);
-        return;
-    }
-
-    // Center text in select box
-    SDL_Rect rect = {xShift + (SEL_WIDTH / 2 - surface->w / 2), yShift + (SEL_HEIGHT / 2 - surface->h / 2), surface->w, surface->h};
-    SDL_RenderCopy(renderer, texture, NULL, &rect);
-
-    SDL_DestroyTexture(texture);
-    SDL_FreeSurface(surface);
-}
-
-void Cg::render()
-{
-    base.render();
-    part1.render();
-    part2.render();
-}
-
-void Cg::clear()
-{
-    assetRaw.clear();
-
-    base.clear();
-    part1.clear();
-    part2.clear();
 }
 
 const json Fw::dump()
@@ -173,15 +75,6 @@ const json Cg::dump()
 {
     auto &j = *this;
     return j;
-}
-
-// Clear the entire canvas
-void ImageManager::clearCanvas()
-{
-    clearImageType(IMAGE_TYPE::BG);
-    clearImageType(IMAGE_TYPE::EG);
-    clearImageType(IMAGE_TYPE::CG);
-    clearImageType(IMAGE_TYPE::FW);
 }
 
 // Load a json object of dumped image data
